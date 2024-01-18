8136604e1c3b61eeeeb7e3cbfec5422893069879
Senjith P <senjithp.98@gmail.com>
2024-01-18 15:31:18 +0530

    update commit

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://www.electromingle.senjith.shop",
   },
 });
 
